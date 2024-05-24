ba5ae61e8e1a07370ba180c76f6102901772435e
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-24 13:19:50 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -3,7 +3,7 @@
 `default_nettype none
 
 // Memory Estimator Interface
-interface MotionEstimationInterface (input bit clk);
+interface MotionEstimationInterface (bit clk);
 
   // Signals
   bit start; 
